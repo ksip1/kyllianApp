64f4b7fdfed308d483292d638e542e237bad4b8a
ksip1 <kyllian.sip@etu.unilim.fr>
2025-04-18 21:25:01 +0200

    initial commit

---
+++ kyllianApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace kyllianApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
